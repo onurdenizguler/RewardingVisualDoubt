b69cdc5c5e8300fb01733067c63e38a8eeb0b9bc
onurdenizguler <onurdenizguler@gmail.com>
2025-02-17 15:39:07 +0100

    feat(dataset): introduce collate fnx

--- src/RewardingVisualDoubt/dataset.py
+++ src/RewardingVisualDoubt/dataset.py
@@ -4,6 +4,7 @@ import typing as T
 from dataclasses import asdict, dataclass
 from pathlib import Path
 
+from huggingface_hub.lfs import TypedDict
 import numpy as np
 import pandas as pd
 import torch
@@ -11,13 +12,14 @@ from LLAVA_Biovil.llava.constants import IMAGE_TOKEN_INDEX
 from LLAVA_Biovil.llava.mm_utils import tokenizer_image_token
 from PIL import Image
 from torch.utils.data import Dataset as TorchDataset
-from torchvision.transforms import Compose
+from torch.nn.utils.rnn import pad_sequence as torch_pad_sequence
 from transformers import PreTrainedTokenizer
 from utils import create_chest_xray_transform_for_inference, remap_to_uint8
 
-from RewardingVisualDoubt import mimic_cxr, shared
+from RewardingVisualDoubt import mimic_cxr
 
 biovil_image_transformer = create_chest_xray_transform_for_inference(512, center_crop_size=448)
+bfloat16_dtype: torch.dtype = torch.bfloat16
 
 
 class DatasetSplit(enum.Enum):
@@ -26,11 +28,26 @@ class DatasetSplit(enum.Enum):
     TEST = "test"
 
 
+class LlavaModelInputDict(T.TypedDict):
+    text_prompt_input_ids: torch.Tensor
+    images: torch.Tensor  # Image tensor of dtype 16
+
+
 # model domain
 @dataclass
 class LlavaModelInput:
     text_prompt_input_ids: torch.Tensor  # Tokenized text
     images: torch.Tensor  # Image tensor
+
+    def as_dict(self) -> LlavaModelInputDict:
+        return T.cast(LlavaModelInputDict, asdict(self))
+
+
+class PromptedMimicCxrLlavaModelInputDatapointDict(T.TypedDict):
+    llava_model_input_dict: LlavaModelInputDict
+    label: bool | None
+    prompt: str
+    mimic_cxr_datapoint_metadata: mimic_cxr.MimicCxrDatapoint
 
 
 # data interface coupled with loading conversion logic
@@ -44,6 +61,15 @@ def _load_image(
     return image
 
 
+def _create_prompt(
+    prompter: T.Callable[[str], str],
+    disease_labels: T.List[mimic_cxr.ChexpertFinding],
+) -> str:
+    findings_string = mimic_cxr.convert_binary_chexpert_findings_to_string(disease_labels)
+    text_input = prompter(findings_string)
+    return text_input
+
+
 # training/inference time logic
 def move_llava_model_input_to_device(
     model_input: LlavaModelInput, device: torch.device
@@ -51,6 +77,14 @@ def move_llava_model_input_to_device(
     model_input.images = model_input.images.to(device, dtype=torch.bfloat16)
     model_input.text_prompt_input_ids = model_input.text_prompt_input_ids.to(device)
     return model_input
+
+
+def move_llava_model_input_dict_to_device(
+    model_input_dict: LlavaModelInputDict, device: torch.device
+) -> LlavaModelInputDict:
+    model_input_dict["images"] = model_input_dict["images"].to(device, dtype=torch.bfloat16)
+    model_input_dict["text_prompt_input_ids"] = model_input_dict["text_prompt_input_ids"].to(device)
+    return model_input_dict
 
 
 # model repository function coupled with datapoint and prompter
@@ -65,18 +99,17 @@ def _create_llava_model_input_from_mimic_cxr_datapoint(
     # Handle image
     image = _load_image(datapoint.img_path)
     if image_transform:
-        image = image_transform(image).unsqueeze(0)
+        image = image_transform(image)  # .unsqueeze(0)
     else:
-        image = torch.tensor(np.array(image)).unsqueeze(0)
+        image = torch.tensor(np.array(image))  # .unsqueeze(0)
 
-    findings_string = mimic_cxr.convert_binary_chexpert_findings_to_string(datapoint.disease_labels)
-    text_input = prompter(findings_string)
+    text_input = _create_prompt(prompter, datapoint.disease_labels)
 
     # get findings from the datapoint
     # Handle text input
     input_ids = tokenizer_image_token(
         text_input, tokenizer, IMAGE_TOKEN_INDEX, return_tensors="pt"
-    ).unsqueeze(0)
+    )  # .unsqueeze(0)
 
     return LlavaModelInput(
         text_prompt_input_ids=input_ids,
@@ -111,24 +144,79 @@ class PromptedMimicCxrLlavaModelInputDataset(TorchDataset):
     prompter: T.Callable[[str], str]
     image_transform: T.Callable[[Image.Image], torch.Tensor] = biovil_image_transformer
     split: DatasetSplit = DatasetSplit.TRAIN
-    device: torch.device = torch.device(shared.torch_devices.cpu.value)
+    supports_label: bool = False
 
     def __post_init__(self):
-        """This runs after dataclass initializes the fields"""
         self.df = self.mimic_cxr_df[self.mimic_cxr_df["split"] == self.split.value]
 
     def __len__(self):
         return len(self.df)
 
-    def __getitem__(self, idx):
+    def __getitem__(self, idx: int) -> PromptedMimicCxrLlavaModelInputDatapointDict:
 
         row = self.df.iloc[idx]
         mimic_cxr_datapoint = _create_mimic_cxr_datapoint_from_mimic_cxr_dataset_df_row(row)
+        prompt = _create_prompt(self.prompter, mimic_cxr_datapoint.disease_labels)
         llava_model_input = _create_llava_model_input_from_mimic_cxr_datapoint(
             mimic_cxr_datapoint, self.tokenizer, self.prompter, self.image_transform
         )
 
-        return asdict(move_llava_model_input_to_device(llava_model_input, self.device))
+        label = True if self.supports_label else None  # TODO fetch label from actual gt
+
+        return PromptedMimicCxrLlavaModelInputDatapointDict(
+            llava_model_input_dict=llava_model_input.as_dict(),
+            label=label,
+            prompt=prompt,
+            mimic_cxr_datapoint_metadata=mimic_cxr_datapoint,
+        )
+
+
+# TODO: Prompt creation logic for random binary question asking + label assignment
+
+
+def prompted_mimic_cxr_llava_model_input_collate_fn(
+    batch: list[PromptedMimicCxrLlavaModelInputDatapointDict], padding_value
+) -> tuple[LlavaModelInputDict, torch.Tensor | list, list[str], list[mimic_cxr.MimicCxrDatapoint]]:
+    """
+    Custom collate function to pad text sequences and stack images.
+    """
+    llava_model_inputs, labels, prompts, mimic_cxr_datapoint_metadatas = zip(
+        *[
+            (
+                item["llava_model_input_dict"],
+                item["label"],
+                item["prompt"],
+                item["mimic_cxr_datapoint_metadata"],
+            )
+            for item in batch
+        ]
+    )
+
+    text_inputs = [
+        torch.as_tensor(sample["text_prompt_input_ids"]).clone().detach()
+        for sample in llava_model_inputs
+    ]
+
+    images = torch.stack(
+        [torch.as_tensor(sample["images"]) for sample in llava_model_inputs]
+    )  # Stack images
+
+    # Pad text sequences to the same length
+    text_inputs = torch_pad_sequence(text_inputs, batch_first=True, padding_value=padding_value)
+
+    # Reconstruct llava_model_input with padded sequences
+    batch_llava_model_inputs = LlavaModelInputDict(text_prompt_input_ids=text_inputs, images=images)
+
+    # Collect labels as a tensor (or list if they are None)
+    batch_labels = (
+        torch.tensor(labels, dtype=torch.float32) if labels[0] is not None else list(labels)
+    )
+
+    # Metadata remains unchanged as a list
+    batch_metadata = list(mimic_cxr_datapoint_metadatas)
+    batch_prompts = list(prompts)
+
+    return batch_llava_model_inputs, batch_labels, batch_prompts, batch_metadata
 
 
 # Probably retire this function
