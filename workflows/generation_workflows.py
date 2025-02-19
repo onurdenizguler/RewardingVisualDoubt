5ef48e9fcf9880132a8fd1947c60ea393dc57260
onurdenizguler <onurdenizguler@gmail.com>
2025-02-19 12:42:32 +0100

    feat(workflows): enrich ppo training sketch, add binary qa generation workflow

--- workflows/generation_workflows.py
+++ workflows/generation_workflows.py
@@ -1,5 +1,4 @@
 # %% Set script for interactive development and import modules
-from encodings.punycode import T
 from IPython.core.getipython import get_ipython
 
 ipython_client = get_ipython()
@@ -8,13 +7,13 @@ if ipython_client:
     ipython_client.run_line_magic(magic_name="autoreload", line="2")
 
 import torch
+from datasets import IterableDataset
 from LLAVA_Biovil.llava.conversation import SeparatorStyle, conv_vicuna_v1
 from LLAVA_Biovil.llava.mm_utils import KeywordsStoppingCriteria
-
 from torch.utils.data import DataLoader
 
-from RewardingVisualDoubt import dataset, inference, mimic_cxr, prompter, shared, vllm
-from datasets import IterableDataset
+from RewardingVisualDoubt import (dataset, inference, mimic_cxr, prompter,
+                                  shared, vllm)
 
 STOP_STR = (
     conv_vicuna_v1.copy().sep
@@ -30,7 +29,7 @@ tokenizer = vllm.load_pretrained_llava_tokenizer_with_image_support(
 # %% load the model
 model = vllm.load_pretrained_llava_model()
 
-# %% load the test split of the mimic-cxr dataset as a PyTorch dataset
+# %% REPORT GENERATION USE-CASE DATASET
 report_generation_prompted_mimic_cxr_llava_model_input_test_dataset = dataset.PromptedMimicCxrLlavaModelInputDataset(
     mimic_cxr_df=mimic_cxr.create_mimic_cxr_dataset_df(),
     tokenizer=tokenizer,
@@ -54,7 +53,7 @@ dataloader = DataLoader(
     persistent_workers=True,
 )
 
-# %% generate a few reports
+# %% GENERATE A FEW REPORTS
 for idx, datapoint in enumerate(dataloader):
     llava_model_input_dict = datapoint[0]
     llava_model_input_dict = dataset.move_llava_model_input_dict_to_device(
@@ -77,12 +76,41 @@ for idx, datapoint in enumerate(dataloader):
         break
 
 
-# %% ask a few binary qa questions
+# %% ASK A FEW BINARY QA QUESTIONS
+tokenizer.pad_token_id = tokenizer.eos_token_id
+dataset_test = dataset.get_binary_qa_prompted_mimic_cxr_llava_model_input_dataset(
+    split=dataset.DatasetSplit.TEST,
+    tokenizer=tokenizer,
+    prompter=prompter.build_binary_qa_instruction_from_disease_under_study,
+)
+dataloader_test = dataset.get_mimic_cxr_llava_model_input_dataloader(
+    dataset=dataset_test, batch_size=1, padding_value=tokenizer.eos_token_id, num_workers=8
+)
 
+for idx, datapoint in enumerate(dataloader_test):
+    llava_model_input_dict = datapoint[0]
+    llava_model_input_dict = dataset.move_llava_model_input_dict_to_device(
+        llava_model_input_dict, torch.device(shared.torch_devices.cuda.value)
+    )
+    input_ids, images = (
+        llava_model_input_dict["text_prompt_input_ids"],
+        llava_model_input_dict["images"],
+    )
+    stopping_criteria = KeywordsStoppingCriteria([STOP_STR], tokenizer, input_ids)
+    pred = inference.generate_radialog_answer_for_binary_qa_for_single_study(
+        model, tokenizer, input_ids, images, stopping_criteria
+    )
+    print(f"\n Metadata: {datapoint[3]}")
+    print(f"Prompt: {datapoint[2]}")
+    print(f"Label:", datapoint[1])
+    print(f"File_idx {idx}, ASSISTANT: ", pred)
+    if idx == 10:
+        break
 
-####################################################################################################
+# %%
+# ####################################################################################################
 # ARCHIVED CODE
-# %% Create dataset via dataset generator into HF Dataset (lazy loading)
+# Create dataset via dataset generator into HF Dataset (lazy loading)
 dataset_generator = dataset.create_dataset_generator_from_mimic_cxr_dataset_df(
     mimic_cxr_df=mimic_cxr.create_mimic_cxr_dataset_df(),
     prompter=prompter.build_report_generation_instruction_from_findings,
--- workflows/radialog_binary_qa_ppo_training.py
+++ workflows/radialog_binary_qa_ppo_training.py
@@ -6,54 +6,200 @@ if ipython_client:
     ipython_client.run_line_magic(magic_name="load_ext", line="autoreload")
     ipython_client.run_line_magic(magic_name="autoreload", line="2")
 
+import pathlib as path
+
 import torch
 from torch.utils.data import DataLoader
+# from LLAVA_Biovil.llava.mm_utils import KeywordsStoppingCriteria
+from trl import AutoModelForCausalLMWithValueHead, PPOConfig, PPOTrainer
 
 from RewardingVisualDoubt import dataset, mimic_cxr, prompter, shared, vllm
 
-# from LLAVA_Biovil.llava.conversation import SeparatorStyle, conv_vicuna_v1
-# from LLAVA_Biovil.llava.mm_utils import KeywordsStoppingCriteria
-# from trl import AutoModelForCausalLMWithValueHead, PPOConfig, PPOTrainer
+DEFAULT_BATCH_SIZE = 8
+DEFAULT_OUTPUT_DIR = path.Path("output")
 
-
+# %% load tokenizer
 tokenizer = vllm.load_pretrained_llava_tokenizer_with_image_support(
     model_base=vllm.LLAVA_BASE_MODEL_NAME
 )
 
 
-# model= vllm.load_pretrained_llava_model()
-
-
 # %%
-binary_qa_prompted_mimic_cxr_llava_model_input_dataset_train = (
-    dataset.BinaryQAPromptedMimicCxrLlavaModelInputDataset(
-        balanced_binary_qa_mimic_cxr_df=mimic_cxr.create_balanced_binary_qa_mimic_cxr_dataset_df(
-            mimic_cxr.create_mimic_cxr_dataset_df()
-        ),
+def train(out_dir: path.Path = DEFAULT_OUTPUT_DIR, batch_size: int = DEFAULT_BATCH_SIZE):
+    # if not os.path.exists(out_dir):
+    #     os.mkdir(out_dir)
+
+    # parameters = {
+    #     "experiment_name": out_dir,
+    #     "model_dir": model_dir,
+    #     "tokenizer_dir": tokenizer_dir,
+    #     "lr": lr,
+    #     "epochs": epochs,
+    #     "episode_length": episode_length,
+    #     "batchsize": batchsize,
+    # }
+    # with open(os.path.join(out_dir, "parameters.json"), "w") as outfile:
+    #     json.dump(parameters, outfile)
+
+    device = (
+        torch.device(shared.torch_devices.cuda.value)
+        if torch.cuda.is_available()
+        else torch.device(shared.torch_devices.cpu.value)
+    )
+
+    model = vllm.load_pretrained_llava_model()
+    model = vllm.add_value_head_to_LlavaLlamaForCausalLM_model(model)
+    model.to(device)
+
+    model_ref = vllm.load_pretrained_llava_model()
+    model_ref = vllm.add_value_head_to_LlavaLlamaForCausalLM_model(model)
+    model_ref.to(device)
+
+    # tokenizer = load_tokenizer(tokenizer_dir) # TODO do i need a tokenizer dir?
+    tokenizer = vllm.load_pretrained_llava_tokenizer_with_image_support(
+        model_base=vllm.LLAVA_BASE_MODEL_NAME
+    )
+    # tokenizer.pad_token = tokenizer.eos_token # TODO do i need to set this? i'm doing it in collate fn
+
+    dataset_train = dataset.get_binary_qa_prompted_mimic_cxr_llava_model_input_dataset(
+        split=dataset.DatasetSplit.TRAIN,
         tokenizer=tokenizer,
         prompter=prompter.build_binary_qa_instruction_from_disease_under_study,
-        image_transform=dataset.biovil_image_transformer,
-        split=dataset.DatasetSplit.TRAIN,
     )
-)
+    dataset_eval = dataset.get_binary_qa_prompted_mimic_cxr_llava_model_input_dataset(
+        split=dataset.DatasetSplit.VALIDATION,
+        tokenizer=tokenizer,
+        prompter=prompter.build_binary_qa_instruction_from_disease_under_study,
+    )
 
-# %%
+    config = PPOConfig(
+        learning_rate=lr,
+        task_name="gpt",
+        batch_size=batch_size,
+        mini_batch_size=batch_size,
+        log_with="wandb",
+        project_kwargs={"logging_dir": out_dir},
+        # cliprange=0.1,
+        # cliprange_value=0.1
+        # adap_kl_ctrl=False,
+        init_kl_coef=0.0001,
+        target=1,
+        # horizon=32,
+        # gamma=0.8
+    )
 
-dataloader = DataLoader(
-    dataset=binary_qa_prompted_mimic_cxr_llava_model_input_dataset_train,
-    batch_size=2,
-    collate_fn=lambda x: dataset.prompted_mimic_cxr_llava_model_input_collate_fn(
-        x, padding_value=tokenizer.eos_token_id
-    ),  # vicuna/llama does not have a padding token, use the EOS token instead
-    shuffle=False,
-    num_workers=4,
-    pin_memory=True,
-    drop_last=True,
-    persistent_workers=True,
-)
+    ppo_trainer = PPOTrainer(
+        model=model,
+        ref_model=model_ref,
+        config=config,
+        dataset=dataset,
+        tokenizer=tokenizer,
+        # optimizer=optimizer
+    )
 
-for idx, batch in enumerate(dataloader):
-    print(batch)
-    break
+    optimizer_sst = AdamW(model.parameters(), lr=5e-5)
 
-# %%
+    generation_kwargs = {
+        "min_length": -1,  # don't ignore the EOS token (see above)
+        "top_k": 0.0,  # no top-k sampling
+        "top_p": 1.0,  # no nucleus sampling
+        "do_sample": True,  # yes, we want to sample
+        "pad_token_id": model.config.eos_token_id,  # most decoder models don't have a padding token - use EOS token instead
+        "max_new_tokens": 32,
+    }
+
+    generation_kwargs_first_part = {
+        "max_new_tokens": 1,
+    }
+
+    best_reward = -100
+    best_reward_epoch = -1
+
+    for epoch in range(epochs):
+        model.train()
+
+        rewards_epoch = []
+        sst_losses = []
+        for idx, batch in enumerate(ppo_trainer.dataloader):
+            inputs = batch["inputs"]
+            # inputs = [i for i in inputs]
+            label_tokens = tokenizer(
+                batch["label"], return_tensors="pt", padding=True
+            ).input_ids.to(device)
+
+            supervised_input = torch.cat((inputs, label_tokens[:, 1].reshape(-1, 1)), dim=-1)
+
+            encoding = {}
+            encoding["input_ids"] = supervised_input
+            encoding["token_type_ids"] = torch.zeros_like(supervised_input)
+            encoding["attention_mask"] = torch.ones_like(supervised_input)
+
+            encoding = {k: v.to(device) for k, v in encoding.items()}
+
+            outputs = model(**encoding, labels=encoding["input_ids"])
+            loss = outputs[1]
+            loss.backward()
+            optimizer_sst.step()
+
+            sst_losses.append(loss.item())
+
+            # generate the prediction without confidence
+            model.eval()
+            prediction = model.generate(inputs, **generation_kwargs_first_part)
+            prediction = [p for p in prediction]
+
+            # generate confidence
+            model.train()
+            response_tensors = ppo_trainer.generate(
+                prediction, return_prompt=False, **generation_kwargs
+            )
+
+            # Create prediction + confidence output
+            total_tensor = [
+                torch.cat((p[-1].unsqueeze(0), c), 0) for p, c in zip(prediction, response_tensors)
+            ]
+            outputs = tokenizer.batch_decode(total_tensor, skip_special_tokens=True)
+
+            # Compute rewards
+            rewards = [reward_function(o, l) for o, l in zip(outputs, batch["label"])]
+            rewards_epoch += rewards
+            rewards = [torch.tensor(r).to(device) for r in rewards]
+
+            # Create log data
+            expected_prediction = [get_expected_label(o) for o in batch["num_ones"]]
+            batch["query"] = expected_prediction
+            batch["response"] = outputs
+
+            try:
+                # stats = ppo_trainer.step(inputs, response_tensors, rewards)
+                stats = ppo_trainer.step(prediction, response_tensors, rewards)
+            except IndexError:
+                print(f"INDEX ERROR detected and ignored {idx}")
+
+                # for i, o in zip(batch["sample"], response_tensors):
+                #     print(f"{i} : {o}")
+
+            ppo_trainer.log_stats(stats, batch, rewards, columns_to_log=["query", "response"])
+
+        avg_reward = np.mean(rewards_epoch)
+
+        mean_sst_loss = np.mean(np.array(sst_losses))
+        print(f"SST Losses: {mean_sst_loss}")
+        wandb.log({"mean_sst_train": mean_sst_loss})
+
+        # scheduler.step(avg_reward)
+        # scheduler.step()
+        print(f"Finished epoch {epoch}. Average reward: {avg_reward}")
+        ppo_trainer.save_pretrained(os.path.join(out_dir, "model_finetuned"))
+        mean_reward = evaluate_model(model, dataset_eval, tokenizer, device)
+        wandb.log({"mean_reward_evaluation": mean_reward})
+        print(f"Evaluation reward: {mean_reward}")
+
+        if mean_reward > best_reward:
+            ppo_trainer.save_pretrained(os.path.join(out_dir, "model_finetuned_best"))
+            best_reward = mean_reward
+            best_reward_epoch = epoch
+
+    print("Finished Training!")
+    print(f"Best avg reward {best_reward} in epoch {best_reward_epoch}")
+    return
