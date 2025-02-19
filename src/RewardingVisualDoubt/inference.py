442960f5f346d6ad840c1e05b12be53f53ea40ea
onurdenizguler <onurdenizguler@gmail.com>
2025-02-19 12:42:51 +0100

    feat(inference): add binary qa answer generation inference support

--- src/RewardingVisualDoubt/inference.py
+++ src/RewardingVisualDoubt/inference.py
@@ -1,7 +1,7 @@
 import torch
 
 
-def generate_report_for_single_study(
+def generate_radialog_report_for_single_study(
     model, tokenizer, input_ids, image_tensor, stopping_criteria
 ) -> str:
 
@@ -19,6 +19,19 @@ def generate_report_for_single_study(
     return pred
 
 
-def binary_qa_for_single_study(model):
-    # TODO
+def generate_radialog_answer_for_binary_qa_for_single_study(
+    model, tokenizer, input_ids, image_tensor, stopping_criteria
+) -> str:
+    with torch.inference_mode():
+        output_ids = model.generate(
+            input_ids,
+            images=image_tensor,
+            do_sample=False,
+            use_cache=True,
+            max_new_tokens=300,
+            stopping_criteria=[stopping_criteria],
+            pad_token_id=tokenizer.pad_token_id,
+        )
+    pred = tokenizer.decode(output_ids[0, input_ids.shape[1] :]).strip().replace("</s>", "")
+    return pred
     pass
