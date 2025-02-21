c1e237467a0568112a9ba19701a98e8194d7ca35
onurdenizguler <onurdenizguler@gmail.com>
2025-02-21 11:34:13 +0100

    feat(workflows): improve the training sketch by integrating more real lines instead of placeholders

--- workflows/radialog_binary_qa_ppo_training.py
+++ workflows/radialog_binary_qa_ppo_training.py
@@ -10,6 +10,7 @@ import pathlib as path
 
 import torch
 from torch.utils.data import DataLoader
+
 # from LLAVA_Biovil.llava.mm_utils import KeywordsStoppingCriteria
 from trl import AutoModelForCausalLMWithValueHead, PPOConfig, PPOTrainer
 
@@ -47,19 +48,25 @@ def train(out_dir: path.Path = DEFAULT_OUTPUT_DIR, batch_size: int = DEFAULT_BAT
         else torch.device(shared.torch_devices.cpu.value)
     )
 
-    model = vllm.load_pretrained_llava_model()
-    model = vllm.add_value_head_to_LlavaLlamaForCausalLM_model(model)
+    model = vllm.load_pretrained_llava_model_for_ppo_training()
     model.to(device)
 
-    model_ref = vllm.load_pretrained_llava_model()
-    model_ref = vllm.add_value_head_to_LlavaLlamaForCausalLM_model(model)
+    model_ref = vllm.load_pretrained_llava_model_for_ppo_training()
     model_ref.to(device)
 
     # tokenizer = load_tokenizer(tokenizer_dir) # TODO do i need a tokenizer dir?
     tokenizer = vllm.load_pretrained_llava_tokenizer_with_image_support(
         model_base=vllm.LLAVA_BASE_MODEL_NAME
     )
-    # tokenizer.pad_token = tokenizer.eos_token # TODO do i need to set this? i'm doing it in collate fn
+
+    # model.config.tokenizer_padding_side = "left"  # RaDialog loading logic handles it alreay
+    # model.padding_side='left' - PAUL DOES IT
+    # model.pad_token_id = tokenizer.eos_token_id - PAUL DOES IT
+
+    padding_tokenizer = vllm.load_pretrained_llava_tokenizer_with_image_support(
+        model_base=vllm.LLAVA_BASE_MODEL_NAME
+    )
+    padding_tokenizer.padding_side = "left"
 
     dataset_train = dataset.get_binary_qa_prompted_mimic_cxr_llava_model_input_dataset(
         split=dataset.DatasetSplit.TRAIN,
@@ -72,129 +79,191 @@ def train(out_dir: path.Path = DEFAULT_OUTPUT_DIR, batch_size: int = DEFAULT_BAT
         prompter=prompter.build_binary_qa_instruction_from_disease_under_study,
     )
 
+    dataloader_train = dataset.get_mimic_cxr_llava_model_input_dataloader(
+        dataset=dataset_train,
+        batch_size=batch_size,
+        padding_tokenizer=padding_tokenizer,
+        # num_workers=4, # Let Torch decide.
+    )
+
+    dataloader_eval = dataset.get_mimic_cxr_llava_model_input_dataloader(
+        dataset=dataset_eval,
+        batch_size=2 * batch_size,
+        padding_tokenizer=padding_tokenizer,
+        # num_workers=4, # Let Torch decide.
+    )
+
     config = PPOConfig(
         learning_rate=lr,
         task_name="gpt",
-        batch_size=batch_size,
-        mini_batch_size=batch_size,
-        log_with="wandb",
+        batch_size=batchsize,
+        mini_batch_size=int(batchsize / 2),
+        log_with=log_with,
         project_kwargs={"logging_dir": out_dir},
-        # cliprange=0.1,
-        # cliprange_value=0.1
-        # adap_kl_ctrl=False,
-        init_kl_coef=0.0001,
-        target=1,
-        # horizon=32,
-        # gamma=0.8
+        remove_unused_columns=False,
+        optimize_device_cache=True,
+        init_kl_coef=0.05,
     )
 
-    ppo_trainer = PPOTrainer(
+    ppo_trainer = PPOTrainerNoCache(
         model=model,
-        ref_model=model_ref,
         config=config,
-        dataset=dataset,
+        dataset=dataset_train,
         tokenizer=tokenizer,
-        # optimizer=optimizer
+        data_collator=data_collator,
     )
 
-    optimizer_sst = AdamW(model.parameters(), lr=5e-5)
+    prediction_terminators = [
+        tokenizer.eos_token_id,
+        tokenizer.convert_tokens_to_ids("<|eot_id|>"),
+        tokenizer.convert_tokens_to_ids("ĠConfidence"),
+    ]
 
-    generation_kwargs = {
+    ppo_terminators = [
+        tokenizer.eos_token_id,
+        tokenizer.convert_tokens_to_ids("<|eot_id|>"),
+    ]
+
+    generation_kwargs_prediction = {
+        "max_new_tokens": 256,
+        "eos_token_id": prediction_terminators,
+        "do_sample": True,
+        "temperature": 0.6,
+        "top_p": 0.9,
+        "pad_token_id": tokenizer.eos_token_id,
+    }
+
+    generation_kwargs_ppo = {
         "min_length": -1,  # don't ignore the EOS token (see above)
         "top_k": 0.0,  # no top-k sampling
         "top_p": 1.0,  # no nucleus sampling
         "do_sample": True,  # yes, we want to sample
-        "pad_token_id": model.config.eos_token_id,  # most decoder models don't have a padding token - use EOS token instead
+        "pad_token_id": tokenizer.eos_token_id,  # most decoder models don't have a padding token - use EOS token instead
         "max_new_tokens": 32,
+        "eos_token_id": ppo_terminators,
+        "max_new_tokens": 500,
     }
 
-    generation_kwargs_first_part = {
-        "max_new_tokens": 1,
+    eval_terminators = [
+        tokenizer.eos_token_id,
+        tokenizer.convert_tokens_to_ids("<|eot_id|>"),
+    ]
+
+    generation_kwargs_eval = {
+        "max_new_tokens": 256,
+        "eos_token_id": eval_terminators,
+        "do_sample": True,
+        "temperature": 0.6,
+        "top_p": 0.9,
+        "pad_token_id": tokenizer.eos_token_id,
     }
+
+    # For random exploration
+    confidences = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    confidences = [str(c) for c in confidences]
+    confidences_tokens = tokenizer.convert_tokens_to_ids(confidences)
+    chance_to_change_confidence = 0
+    steps_to_reach_zero = len(ppo_trainer.dataloader)
+    reduce_per_step = chance_to_change_confidence / steps_to_reach_zero
 
     best_reward = -100
     best_reward_epoch = -1
 
     for epoch in range(epochs):
-        model.train()
 
         rewards_epoch = []
-        sst_losses = []
         for idx, batch in enumerate(ppo_trainer.dataloader):
-            inputs = batch["inputs"]
-            # inputs = [i for i in inputs]
-            label_tokens = tokenizer(
-                batch["label"], return_tensors="pt", padding=True
-            ).input_ids.to(device)
 
-            supervised_input = torch.cat((inputs, label_tokens[:, 1].reshape(-1, 1)), dim=-1)
+            input_ids = batch["input_ids"].to(device)
+            attention_mask = batch["attention_mask"].to(device)
+            gt_candidates = batch["gt_candidates"]
+            questions = batch["question"]
+            is_multiple_choice = batch["is_multiple_choice"]
 
-            encoding = {}
-            encoding["input_ids"] = supervised_input
-            encoding["token_type_ids"] = torch.zeros_like(supervised_input)
-            encoding["attention_mask"] = torch.ones_like(supervised_input)
+            if is_unsloth:
+                FastLanguageModel.for_inference(model.pretrained_model)
+            else:
+                model.eval()
 
-            encoding = {k: v.to(device) for k, v in encoding.items()}
+            prediction = model.generate(
+                input_ids=input_ids, attention_mask=attention_mask, **generation_kwargs_prediction
+            )
+            prediction = [remove_padding(p, tokenizer.pad_token_id) for p in prediction]
 
-            outputs = model(**encoding, labels=encoding["input_ids"])
-            loss = outputs[1]
-            loss.backward()
-            optimizer_sst.step()
-
-            sst_losses.append(loss.item())
-
-            # generate the prediction without confidence
-            model.eval()
-            prediction = model.generate(inputs, **generation_kwargs_first_part)
-            prediction = [p for p in prediction]
-
-            # generate confidence
-            model.train()
+            # Generate confidence
+            if not is_unsloth:
+                model.train()
             response_tensors = ppo_trainer.generate(
-                prediction, return_prompt=False, **generation_kwargs
+                prediction, return_prompt=False, **generation_kwargs_ppo
             )
 
             # Create prediction + confidence output
-            total_tensor = [
-                torch.cat((p[-1].unsqueeze(0), c), 0) for p, c in zip(prediction, response_tensors)
+            total_tensor = [torch.cat((p, c), 0) for p, c in zip(prediction, response_tensors)]
+            answer_only_tensor = [
+                total_tensor[i][len(input_ids[i]) :] for i in range(len(input_ids))
             ]
-            outputs = tokenizer.batch_decode(total_tensor, skip_special_tokens=True)
+
+            # For random exploration
+            if np.random.random() < chance_to_change_confidence:
+                answer_only_tensor = [
+                    change_confidence(a, confidences_tokens, np.random.choice(confidences_tokens))
+                    for a in answer_only_tensor
+                ]
+
+            responses_decoded = tokenizer.batch_decode(answer_only_tensor, skip_special_tokens=True)
+
+            # Parse prediction and confidence
+            results = [
+                response_to_QAResult(question, response, gt, is_mc)
+                for question, response, gt, is_mc in zip(
+                    questions, responses_decoded, gt_candidates, is_multiple_choice
+                )
+            ]
 
             # Compute rewards
-            rewards = [reward_function(o, l) for o, l in zip(outputs, batch["label"])]
+            rewards = [QAResult_to_reward(r) for r in results]
             rewards_epoch += rewards
             rewards = [torch.tensor(r).to(device) for r in rewards]
 
             # Create log data
-            expected_prediction = [get_expected_label(o) for o in batch["num_ones"]]
-            batch["query"] = expected_prediction
-            batch["response"] = outputs
+            batch["response"] = responses_decoded
+            batch["query"] = batch["question"]
 
             try:
-                # stats = ppo_trainer.step(inputs, response_tensors, rewards)
+                if is_unsloth:
+                    FastLanguageModel.for_training(model.pretrained_model)
                 stats = ppo_trainer.step(prediction, response_tensors, rewards)
             except IndexError:
                 print(f"INDEX ERROR detected and ignored {idx}")
 
-                # for i, o in zip(batch["sample"], response_tensors):
-                #     print(f"{i} : {o}")
+            ppo_trainer.log_stats(
+                stats, batch, rewards, columns_to_log=["query", "response", "answer"]
+            )
 
-            ppo_trainer.log_stats(stats, batch, rewards, columns_to_log=["query", "response"])
+            # For random exploration
+            chance_to_change_confidence -= reduce_per_step
+            chance_to_change_confidence = max(0, chance_to_change_confidence)
 
         avg_reward = np.mean(rewards_epoch)
 
-        mean_sst_loss = np.mean(np.array(sst_losses))
-        print(f"SST Losses: {mean_sst_loss}")
-        wandb.log({"mean_sst_train": mean_sst_loss})
-
-        # scheduler.step(avg_reward)
-        # scheduler.step()
         print(f"Finished epoch {epoch}. Average reward: {avg_reward}")
         ppo_trainer.save_pretrained(os.path.join(out_dir, "model_finetuned"))
-        mean_reward = evaluate_model(model, dataset_eval, tokenizer, device)
-        wandb.log({"mean_reward_evaluation": mean_reward})
-        print(f"Evaluation reward: {mean_reward}")
 
+        # Evaluate model after each epoch
+        if is_unsloth:
+            FastLanguageModel.for_inference(model.pretrained_model)
+        else:
+            model.eval()
+        mean_reward, std_reward = evaluate_model(
+            model, dataloader_validation, tokenizer, generation_kwargs_eval, device
+        )
+        if log_with == "wandb":
+            wandb.log({"mean_reward_evaluation": mean_reward})
+            wandb.log({"std_reward_evaluation": std_reward})
+            wandb.log({"exploration_prob": chance_to_change_confidence})
+
+        # Save the best performing model
+        mean_reward = avg_reward
         if mean_reward > best_reward:
             ppo_trainer.save_pretrained(os.path.join(out_dir, "model_finetuned_best"))
             best_reward = mean_reward
