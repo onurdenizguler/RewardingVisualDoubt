55ffa2b543e119482ee5bee66d303b05f6147fd3
onurdenizguler <onurdenizguler@gmail.com>
2025-02-24 08:29:38 +0100

    refactor(workflows): bring ppo training closer to its final form

--- workflows/radialog_binary_qa_ppo_training.py
+++ workflows/radialog_binary_qa_ppo_training.py
@@ -2,23 +2,196 @@
 from RewardingVisualDoubt import infrastructure
 
 infrastructure.make_ipython_reactive_to_changing_codebase()
+infrastructure.supress_known_warnings()
 
 import pathlib as path
+import typing as t
+import os
+import dataclasses
 
+import numpy as np
 import torch
-from torch.utils.data import DataLoader
+from LLAVA_Biovil.llava.mm_utils import KeywordsStoppingCriteria
+import trl
+import transformers
 
-# from LLAVA_Biovil.llava.mm_utils import KeywordsStoppingCriteria
-from trl import AutoModelForCausalLMWithValueHead, PPOConfig, PPOTrainer
-
-from RewardingVisualDoubt import dataset, mimic_cxr, prompter, shared, vllm
+from RewardingVisualDoubt import dataset, prompter, response, reward, shared, vllm
+from RewardingVisualDoubt import train as training
 
 DEFAULT_BATCH_SIZE = 8
 DEFAULT_OUTPUT_DIR = path.Path("output")
+STEPS_UNTIL_CHECKPOINT = 50
+NUM_BATCHES_TO_EVALUATE = 15
+
+STOP_STR = prompter.Seperator.END_OF_SEQUENCE_SEPERATOR.value
+
+
+@dataclasses.dataclass
+class Hyperparameters:
+    num_epochs: int
+    batch_size: int
+    episode_length: int
+    learning_rate: float
+    reward_scaler: float
+
+
+def calculate_mean_and_std_rewards(eval_batch_rewards_list):
+    pass
+
+
+def radialog_binary_qa_ppo_evaluation_step(model, batch):
+    pass
+
+
+def radialog_binary_qa_ppo_training_step(
+    model: trl.AutoModelForCausalLMWithValueHead,
+    device: torch.device,
+    tokenizer: transformers.PreTrainedTokenizer,
+    generation_kwargs_ppo: dict,
+    ppo_trainer: trl.PPOTrainer,
+    batch: dataset.MimicCxrLlavaModelInputBatchDict,
+):
+
+    ######### 5.1 Unpack the batch #########
+    batch = t.cast(dataset.MimicCxrLlavaModelInputBatchDict, batch)
+    batch_llava_model_input_dict = batch["batch_llava_model_input_dict"]
+    batch_llava_model_input_dict = dataset.move_llava_model_input_dict_to_device(
+        batch_llava_model_input_dict, device
+    )
+    input_ids, images = (
+        batch_llava_model_input_dict["text_prompt_input_ids"],
+        batch_llava_model_input_dict["images"],
+    )
+    attention_mask = batch["batch_attention_mask"].to(device)
+    labels = t.cast(torch.Tensor, batch["batch_labels"]).to(device)
+    stopping_criteria = KeywordsStoppingCriteria([STOP_STR], tokenizer, input_ids)
+
+    ######### 5.2 Generate the binary q&a answer and remove trailing padding tokens #########
+    model.eval()
+    prompt_and_generated_answers_ids = model.generate(
+        input_ids=input_ids,
+        images=images,
+        attention_mask=attention_mask,
+        do_sample=False,
+        use_cache=True,
+        max_new_tokens=300,
+        stopping_criteria=[stopping_criteria],
+        pad_token_id=tokenizer.pad_token_id,
+    )
+
+    prompt_and_generated_answers_ids = training.remove_trailing_padding_from_prediction(
+        prompt_and_generated_answers_ids, tokenizer.pad_token_id
+    )
+
+    ######### 5.3 Append confidence request to the generated answers #########
+    prompt_and_generated_answers_with_confidence_requests_ids = []
+    for item in prompt_and_generated_answers_ids:
+        confidence_request_input_ids = (
+            tokenizer(
+                prompter.build_post_generation_user_confidence_request(),
+                return_tensors="pt",
+            )
+            .input_ids.to(device)
+            .squeeze(0)
+        )[
+            1:
+        ]  # drop start of sequence token
+        prompt_and_generated_answers_with_confidence_requests_ids.append(
+            torch.cat((item, confidence_request_input_ids), 0)
+        )
+
+    ######### 5.4 Generate the confidences #########
+    model.train()
+    generated_confidences_ids = ppo_trainer.generate(
+        prompt_and_generated_answers_with_confidence_requests_ids,  # ppo_trainer.generate() method admits list of tensors, not a batch tensor unfortunately
+        images=images,
+        return_prompt=False,
+        **generation_kwargs_ppo,
+    )
+
+    ######### 5.5 Arrange all generations into useful variables #########
+    complete_conversation_ids = [
+        torch.cat((p, c), 0)
+        for p, c in zip(
+            prompt_and_generated_answers_with_confidence_requests_ids,
+            generated_confidences_ids,
+        )
+    ]
+    generated_answer_only_ids = [
+        prompt_and_generated_answers_ids[i][len(input_ids[i]) :] for i in range(len(input_ids))
+    ]
+    prompt_and_generated_answers_with_confidence_requests_ids = (
+        training.replace_image_token_with_another_token_for_list_of_tensors(
+            prompt_and_generated_answers_with_confidence_requests_ids
+        )
+    )
+    generated_answers_texts = tokenizer.batch_decode(
+        generated_answer_only_ids,
+        skip_special_tokens=True,
+    )
+    generated_confidences_texts = tokenizer.batch_decode(
+        generated_confidences_ids,
+        skip_special_tokens=True,
+    )
+
+    ######### 5.6 Parse the responses and compute the rewards #########
+    generated_answer_labels = response.parse_binary_labels(generated_answers_texts)
+    generated_confidence_values = response.parse_confidences(generated_confidences_texts)
+
+    rewards = [
+        reward.generated_answer_and_confidence_to_reward(
+            generated_answer_label, generated_confidence_value, ground_truth_label
+        )
+        for generated_answer_label, generated_confidence_value, ground_truth_label in zip(
+            generated_answer_labels, generated_confidence_values, labels.bool().tolist()
+        )
+    ]
+
+    rewards = t.cast(
+        list[torch.FloatTensor],
+        [torch.tensor(r).to(device) for r in rewards],
+    )
+
+    ######### 5.7 Take a PPO optimization step #########
+    stats = ppo_trainer.step(
+        queries=t.cast(
+            list[torch.LongTensor],
+            prompt_and_generated_answers_with_confidence_requests_ids,
+        ),
+        responses=t.cast(list[torch.LongTensor], generated_answer_only_ids),
+        scores=rewards,
+    )
+
+    ######### 5.8 Create a report and log the training stats #########
+    batch_report = {}
+    # batch_report["complete_conversation_text"] = tokenizer.batch_decode(
+    #     training.replace_image_token_with_another_token_for_list_of_tensors(
+    #         complete_conversation_ids
+    #     ),
+    #     skip_special_tokens=True,
+    # )
+    batch_report["query"] = tokenizer.batch_decode(
+        prompt_and_generated_answers_with_confidence_requests_ids, skip_special_tokens=True
+    )
+    batch_report["response"] = generated_confidence_values
+    # batch_report["generated_answer_labels"] = generated_answer_labels
+    batch_report["generated_answers_texts"] = generated_answers_texts
+    batch_report["generated_confidences_texts"] = generated_confidences_texts
+
+    ppo_trainer.log_stats(
+        stats=stats, batch=batch_report, rewards=rewards
+    )  # The game logs will not be logged because the batch does not contain the keys 'query' and 'response'
+
+    return rewards, batch_report
 
 
 # %%
-def train(out_dir: path.Path = DEFAULT_OUTPUT_DIR, batch_size: int = DEFAULT_BATCH_SIZE):
+def train(
+    num_epochs: int,
+    batch_size: int,
+    learning_rate: float,
+    out_dir: path.Path,
+):
 
     ######################################## 0. Define the environment ########################################
 
@@ -48,7 +221,6 @@ def train(out_dir: path.Path = DEFAULT_OUTPUT_DIR, batch_size: int = DEFAULT_BAT
     ######################################## 1. Load the model and tokenizer ########################################
 
     model = vllm.load_pretrained_llava_model_for_ppo_training(device_str=device_str)
-    model_ref = vllm.load_pretrained_llava_model_for_ppo_training(device_str=device_str)
 
     tokenizer = vllm.load_pretrained_llava_tokenizer_with_image_support(
         model_base=vllm.LLAVA_BASE_MODEL_NAME
@@ -57,15 +229,11 @@ def train(out_dir: path.Path = DEFAULT_OUTPUT_DIR, batch_size: int = DEFAULT_BAT
         model_base=vllm.LLAVA_BASE_MODEL_NAME
     )
     padding_tokenizer.padding_side = "left"
-
-    # TODO do i need a tokenizer dir? # tokenizer = load_tokenizer(tokenizer_dir)
-    # TODO: need padding from the left???
-    #### model.config.tokenizer_padding_side = "left"  # RaDialog loading logic handles it alreay
-    #### model.padding_side='left' - PAUL DOES IT
-    #### model.pad_token_id = tokenizer.eos_token_id - PAUL DOES IT
+    padding_tokenizer.pad_token = padding_tokenizer.bos_token
 
     ######################################## 2. Load the datasets and the dataloaders ########################################
 
+    print("Loading the datasets and the dataloaders...")
     dataset_train = dataset.get_binary_qa_prompted_mimic_cxr_llava_model_input_dataset(
         split=dataset.DatasetSplit.TRAIN,
         tokenizer=tokenizer,
@@ -81,194 +249,118 @@ def train(out_dir: path.Path = DEFAULT_OUTPUT_DIR, batch_size: int = DEFAULT_BAT
         dataset=dataset_train,
         batch_size=batch_size,
         padding_tokenizer=padding_tokenizer,
-        # num_workers=4, # Let Torch decide.
+        num_workers=8,
     )
 
     dataloader_eval = dataset.get_mimic_cxr_llava_model_input_dataloader(
         dataset=dataset_eval,
         batch_size=2 * batch_size,
         padding_tokenizer=padding_tokenizer,
-        # num_workers=4, # Let Torch decide.
+        num_workers=8,
     )
+
+    eval_batch_iterator = iter(dataloader_eval)
 
     ######################################## 3. Define the PPO and generation configurations ########################################
 
-    config = PPOConfig(
-        learning_rate=lr,
+    ppo_config = trl.PPOConfig(
+        learning_rate=learning_rate,
         task_name="gpt",
-        batch_size=batchsize,
-        mini_batch_size=int(batchsize / 2),
-        log_with=log_with,
-        project_kwargs={"logging_dir": out_dir},
+        batch_size=batch_size,
+        mini_batch_size=int(batch_size / 4),
+        # log_with=log_with,
+        # project_kwargs={"logging_dir": out_dir},
         remove_unused_columns=False,
-        optimize_device_cache=True,
+        # optimize_device_cache=True,
         init_kl_coef=0.05,
     )
-
-    prediction_terminators = [
-        tokenizer.eos_token_id,
-        tokenizer.convert_tokens_to_ids("<|eot_id|>"),
-        tokenizer.convert_tokens_to_ids("ĠConfidence"),
-    ]
-
-    ppo_terminators = [
-        tokenizer.eos_token_id,
-        tokenizer.convert_tokens_to_ids("<|eot_id|>"),
-    ]
-
-    generation_kwargs_prediction = {
-        "max_new_tokens": 256,
-        "eos_token_id": prediction_terminators,
-        "do_sample": True,
-        "temperature": 0.6,
-        "top_p": 0.9,
-        "pad_token_id": tokenizer.eos_token_id,
-    }
 
     generation_kwargs_ppo = {
         "min_length": -1,  # don't ignore the EOS token (see above)
         "top_k": 0.0,  # no top-k sampling
         "top_p": 1.0,  # no nucleus sampling
+        "temperature": 0.5,  # DONT BE CREATIVE
         "do_sample": True,  # yes, we want to sample
-        "pad_token_id": tokenizer.eos_token_id,  # most decoder models don't have a padding token - use EOS token instead
-        "max_new_tokens": 32,
-        "eos_token_id": ppo_terminators,
-        "max_new_tokens": 500,
-    }
-
-    eval_terminators = [
-        tokenizer.eos_token_id,
-        tokenizer.convert_tokens_to_ids("<|eot_id|>"),
-    ]
-
-    generation_kwargs_eval = {
-        "max_new_tokens": 256,
-        "eos_token_id": eval_terminators,
-        "do_sample": True,
-        "temperature": 0.6,
-        "top_p": 0.9,
-        "pad_token_id": tokenizer.eos_token_id,
+        "pad_token_id": tokenizer.pad_token_id,  # most decoder models don't have a padding token - use EOS token instead (for this tokenizer it was already set to eos_token_id)
+        "max_new_tokens": 50,  # let's not be chatty, we need a few tokens to generate confidence but also not limit the response too much
+        "eos_token_id": tokenizer.eos_token_id,  # (instead of ppo_terminators list)
     }
 
     ######################################## 4. Get trainer and set training aspirations ########################################
 
-    ppo_trainer = PPOTrainerNoCache(
-        model=model,
-        config=config,
-        dataset=dataset_train,
-        tokenizer=tokenizer,
-        data_collator=data_collator,
+    ppo_trainer = t.cast(
+        trl.PPOTrainer,
+        trl.PPOTrainer(
+            model=model,
+            config=ppo_config,
+            tokenizer=tokenizer,
+        ),
     )
-
-    # For random exploration
-    confidences = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-    confidences = [str(c) for c in confidences]
-    confidences_tokens = tokenizer.convert_tokens_to_ids(confidences)
-    chance_to_change_confidence = 0
-    steps_to_reach_zero = len(ppo_trainer.dataloader)
-    reduce_per_step = chance_to_change_confidence / steps_to_reach_zero
-
-    best_reward = -100
-    best_reward_epoch = -1
 
     ######################################## 5. Train the model ########################################
 
-    for epoch in range(epochs):
+    for epoch in range(num_epochs):
 
-        rewards_epoch = []
-        for idx, batch in enumerate(ppo_trainer.dataloader):
+        best_reward = -100
+        best_reward_epoch = -1
+        rewards_until_checkpoint = []
 
-            input_ids = batch["input_ids"].to(device)
-            attention_mask = batch["attention_mask"].to(device)
-            gt_candidates = batch["gt_candidates"]
-            questions = batch["question"]
-            is_multiple_choice = batch["is_multiple_choice"]
+        for step, batch in enumerate(dataloader_train):
 
-            model.eval()
-
-            prediction = model.generate(
-                input_ids=input_ids, attention_mask=attention_mask, **generation_kwargs_prediction
+            ######### 5.1 - 5.8 Perform a training step #########
+            rewards, batch_report = radialog_binary_qa_ppo_training_step(
+                model=model,
+                device=device,
+                tokenizer=tokenizer,
+                generation_kwargs_ppo=generation_kwargs_ppo,
+                ppo_trainer=ppo_trainer,
+                batch=batch,
             )
-            prediction = [remove_padding(p, tokenizer.pad_token_id) for p in prediction]
+            rewards_until_checkpoint += [r.item() for r in rewards]
 
-            # Generate confidence
-            model.train()
-            response_tensors = ppo_trainer.generate(
-                prediction, return_prompt=False, **generation_kwargs_ppo
-            )
+            ######### 5.9 Checkpoint the model if checkpoint step is reached #########
+            if (step + 1) % STEPS_UNTIL_CHECKPOINT == 0:
 
-            # Create prediction + confidence output
-            total_tensor = [torch.cat((p, c), 0) for p, c in zip(prediction, response_tensors)]
-            answer_only_tensor = [
-                total_tensor[i][len(input_ids[i]) :] for i in range(len(input_ids))
-            ]
+                avg_reward = np.mean(rewards_until_checkpoint)
 
-            # For random exploration
-            if np.random.random() < chance_to_change_confidence:
-                answer_only_tensor = [
-                    change_confidence(a, confidences_tokens, np.random.choice(confidences_tokens))
-                    for a in answer_only_tensor
-                ]
+                print(f"Arrived at checkpoint {step + 1}. Average reward: {avg_reward}")
+                print("Saving the model checkpoint...")
+                ppo_trainer.save_pretrained(os.path.join(out_dir, "model_finetuned"))
 
-            responses_decoded = tokenizer.batch_decode(answer_only_tensor, skip_special_tokens=True)
+                print(f"Running evaluation at checkpoint {step + 1}")
+                model.eval()
+                eval_batch_rewards_list = []
+                for _ in range(NUM_BATCHES_TO_EVALUATE):
+                    try:
+                        eval_batch = next(eval_batch_iterator)
+                    except StopIteration:
+                        eval_batch_iterator = iter(dataloader_eval)
+                        eval_batch = next(eval_batch_iterator)
 
-            # Parse prediction and confidence
-            results = [
-                response_to_QAResult(question, response, gt, is_mc)
-                for question, response, gt, is_mc in zip(
-                    questions, responses_decoded, gt_candidates, is_multiple_choice
-                )
-            ]
+                    eval_batch_rewards = radialog_binary_qa_ppo_evaluation_step(
+                        model, eval_batch
+                    )  # TODO implement
+                    eval_batch_rewards_list.append(eval_batch_rewards)
 
-            # Compute rewards
-            rewards = [QAResult_to_reward(r) for r in results]
-            rewards_epoch += rewards
-            rewards = [torch.tensor(r).to(device) for r in rewards]
+                # mean_reward, std_reward = calculate_mean_and_std_rewards(eval_batch_rewards_list) # TODO implement
 
-            # Create log data
-            batch["response"] = responses_decoded
-            batch["query"] = batch["question"]
+                # if log_with == "wandb":
+                #     wandb.log({"mean_reward_evaluation": mean_reward})
+                #     wandb.log({"std_reward_evaluation": std_reward})
+                #     wandb.log({"exploration_prob": chance_to_change_confidence})
 
-            try:
-                if is_unsloth:
-                    FastLanguageModel.for_training(model.pretrained_model)
-                stats = ppo_trainer.step(prediction, response_tensors, rewards)
-            except IndexError:
-                print(f"INDEX ERROR detected and ignored {idx}")
+                # Save the best performing model
+                mean_reward = avg_reward
+                if mean_reward > best_reward:
+                    ppo_trainer.save_pretrained(os.path.join(out_dir, "model_finetuned_best"))
+                    best_reward = mean_reward
+                    best_reward_epoch = epoch
 
-            ppo_trainer.log_stats(
-                stats, batch, rewards, columns_to_log=["query", "response", "answer"]
-            )
-
-            # For random exploration
-            chance_to_change_confidence -= reduce_per_step
-            chance_to_change_confidence = max(0, chance_to_change_confidence)
-
-        avg_reward = np.mean(rewards_epoch)
-
-        print(f"Finished epoch {epoch}. Average reward: {avg_reward}")
-        ppo_trainer.save_pretrained(os.path.join(out_dir, "model_finetuned"))
-
-        # Evaluate model after each epoch
-        if is_unsloth:
-            FastLanguageModel.for_inference(model.pretrained_model)
-        else:
-            model.eval()
-        mean_reward, std_reward = evaluate_model(
-            model, dataloader_validation, tokenizer, generation_kwargs_eval, device
-        )
-        if log_with == "wandb":
-            wandb.log({"mean_reward_evaluation": mean_reward})
-            wandb.log({"std_reward_evaluation": std_reward})
-            wandb.log({"exploration_prob": chance_to_change_confidence})
-
-        # Save the best performing model
-        mean_reward = avg_reward
-        if mean_reward > best_reward:
-            ppo_trainer.save_pretrained(os.path.join(out_dir, "model_finetuned_best"))
-            best_reward = mean_reward
-            best_reward_epoch = epoch
-
-    print("Finished Training!")
-    print(f"Best avg reward {best_reward} in epoch {best_reward_epoch}")
+    # print("Finished Training!")
+    # print(f"Best avg reward {best_reward} in epoch {best_reward_epoch}")
     return
+
+
+# get the single value from the single dim tensor
+def get_value_from_tensor(tensor):
+    return tensor.item()
