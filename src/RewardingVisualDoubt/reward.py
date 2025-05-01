eabb4ee90db331c31c7448a72609e69b98d5399f
onurdenizguler <onurdenizguler@gmail.com>
2025-05-01 23:07:21 +0200

    feat(reward): add different reward types

--- src/RewardingVisualDoubt/reward.py
+++ src/RewardingVisualDoubt/reward.py
@@ -1,4 +1,5 @@
 import math
+import typing as t
 
 MAX_REWARD = -0.0010005003335835344
 MIN_REWARD = -6.907755278982137 / 2
@@ -6,8 +7,8 @@ SCALE = 5.0
 WRONG_FORMAT_PENALTY = -SCALE * 3.0
 
 
-def reward_function(confidence: int | None, is_answer_correct: bool) -> float:
-    if confidence is None:
+def default_reward_function(confidence: int | None, is_answer_correct: bool | None) -> float:
+    if confidence is None or is_answer_correct is None:
         return WRONG_FORMAT_PENALTY
 
     normalized_confidence = min(0.999, max(0.001, confidence / 10))
@@ -23,12 +24,51 @@ def reward_function(confidence: int | None, is_answer_correct: bool) -> float:
     return float(SCALE * norm_score)
 
 
+def reward_extreme_confidence(confidence: int, **kwargs) -> float:
+    if confidence is None:
+        return WRONG_FORMAT_PENALTY
+
+    if confidence > 7:
+        score = 5.0
+    else:
+        score = 0.5
+    return score
+
+
+def reward_teachers_pet_behaviour(
+    confidence: int, is_answer_correct: bool | None, **kwargs
+) -> float:
+    """
+    Train a student to be a 'teacher's pet' by rewarding high confidence in correct answers and low confidence in incorrect ones.
+    """
+    if confidence is None or is_answer_correct is None:
+        return WRONG_FORMAT_PENALTY
+
+    if is_answer_correct:
+        if confidence > 6:
+            score = 5.0
+        else:
+            score = 0.5
+    else:
+        if confidence < 4:
+            score = 5.0
+        else:
+            score = 0.5
+
+    return score
+
+
 def generated_answer_and_confidence_to_reward(
     generated_answer_label: bool | None,
     generated_confidence_value: int | None,
     ground_truth_label: bool,
+    reward_function: t.Callable = default_reward_function,
 ) -> float:
 
-    is_correct = generated_answer_label == ground_truth_label
-    reward = reward_function(generated_confidence_value, is_correct)
+    is_answer_correct = (
+        generated_answer_label == ground_truth_label if generated_answer_label is not None else None
+    )
+    reward = reward_function(
+        confidence=generated_confidence_value, is_answer_correct=is_answer_correct
+    )
     return reward
