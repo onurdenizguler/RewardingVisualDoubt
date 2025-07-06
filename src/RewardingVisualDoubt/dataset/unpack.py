667fbecbbc7105f7c5803562745804a0a361fa72
onurdenizguler <onurdenizguler@gmail.com>
2025-07-06 15:40:46 +0200

    feat(dataset): unpacking logic for batches

--- src/RewardingVisualDoubt/dataset/unpack.py
+++ src/RewardingVisualDoubt/dataset/unpack.py
@@ -3,9 +3,9 @@ import typing as t
 import torch
 import transformers
 
+from RewardingVisualDoubt import prompter, shared
 
 from . import dataset
-from RewardingVisualDoubt import prompter, shared
 
 
 def unpack_binary_qa_batch(
@@ -35,3 +35,55 @@ def unpack_binary_qa_batch(
         input_ids
     )  # WARNING: Is performed, as the ppo_trainer.generate() method handles padding and batching itself
     return input_ids, images, labels, stopping_criteria, input_ids_list
+
+
+def unpack_binary_qa_batch_with_attention_mask(
+    device: torch.device,
+    tokenizer: transformers.PreTrainedTokenizer,
+    batch: dataset.MimicCxrLlavaModelInputBatchDict,
+) -> tuple[
+    torch.Tensor,
+    torch.Tensor,
+    torch.Tensor,
+    shared.KeywordsStoppingCriteria,
+    torch.Tensor,
+]:
+
+    batch_llava_model_input_dict = batch["batch_llava_model_input_dict"]
+    batch_llava_model_input_dict = dataset.move_llava_model_input_dict_to_device(
+        batch_llava_model_input_dict, device
+    )
+    input_ids, images = (
+        batch_llava_model_input_dict["text_prompt_input_ids"],
+        batch_llava_model_input_dict["images"],
+    )
+    labels = t.cast(torch.Tensor, batch["batch_labels"]).to(device)
+    stopping_criteria = shared.KeywordsStoppingCriteria([prompter.STOP_STR], tokenizer, input_ids)
+    attention_mask = batch["batch_attention_mask"].to(device)
+    return input_ids, images, labels, stopping_criteria, attention_mask
+
+
+def unpack_report_generation_batch_with_attention_mask(
+    device: torch.device | str,
+    tokenizer: transformers.PreTrainedTokenizer,
+    batch: dataset.MimicCxrLlavaModelInputBatchDict,
+) -> tuple[
+    torch.Tensor,
+    torch.Tensor,
+    shared.KeywordsStoppingCriteria,
+    torch.Tensor,
+]:
+    if isinstance(device, str):
+        device = torch.device(device)
+
+    batch_llava_model_input_dict = batch["batch_llava_model_input_dict"]
+    batch_llava_model_input_dict = dataset.move_llava_model_input_dict_to_device(
+        batch_llava_model_input_dict, device
+    )
+    input_ids, images = (
+        batch_llava_model_input_dict["text_prompt_input_ids"],
+        batch_llava_model_input_dict["images"],
+    )
+    stopping_criteria = shared.KeywordsStoppingCriteria([prompter.STOP_STR], tokenizer, input_ids)
+    attention_mask = batch["batch_attention_mask"].to(device)
+    return input_ids, images, stopping_criteria, attention_mask
