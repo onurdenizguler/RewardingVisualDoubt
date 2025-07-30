3aeecc3672fa2ead3a7373b4765dac4ce1b06a3a
onurdenizguler <onurdenizguler@gmail.com>
2025-07-30 17:18:38 +0200

    feat(training): introducing ppo steps draft for radialog report generation

--- src/RewardingVisualDoubt/training/ppo_steps.py
+++ src/RewardingVisualDoubt/training/ppo_steps.py
@@ -258,3 +258,165 @@ def _handle_random_confidence_replacement(
         old_generated_confidence_values,
         is_confidence_randomly_replaced,
     )
+
+
+####################################################################################################################
+# REPORT GENERATION STEPS
+####################################################################################################################
+
+
+def radialog_report_generation_ppo_training_step(
+    model: trl.AutoModelForCausalLMWithValueHead,
+    device: torch.device,
+    tokenizer: transformers.PreTrainedTokenizer,
+    generation_kwargs_ppo: dict,
+    ppo_trainer: llava_ppo.MultimodalPPOTrainer,
+    batch: dataset.MimicCxrLlavaModelInputBatchDict,
+    reward_function: t.Callable,
+    accumulating_game_logs: reinforcement.GameLogs | None = None,
+    chance_to_change_confidence: float = 0.5,  # Default value: every 2nd batch
+    granular_confidence: bool = False,
+    log_calibration_plot: bool = False,
+) -> list[torch.FloatTensor]:
+
+    ######### 5.1 Unpack the batch #########
+
+    input_ids, input_ids_list, images, stopping_criteria, batch_metadata_list = (
+        dataset.unpack_report_generation_batch_with_attention_mask_and_metadata_for_ppo(
+            device=device,
+            tokenizer=tokenizer,
+            batch=batch,
+            postprocessor=postprocessing.remove_preciding_padding_from_batch_tensor,
+        )
+    )
+
+    ######### 5.2 Generate the binary q&a answer and remove trailing padding tokens #########
+    model.eval()
+    model.gradient_checkpointing_disable()
+    generated_ids = ppo_trainer.generate(
+        query_tensor=input_ids_list,  # ppo_trainer.generate() method admits list of tensors, handles padding and batching itself
+        images=images,
+        return_prompt=False,
+        batch_size=input_ids.shape[0],
+        use_cache=True,
+        stopping_criteria=[stopping_criteria],
+        **generation_kwargs_ppo,
+    )
+
+    ######### 5.3 Parse the responses and compute the scores #########
+    generated_texts = tokenizer.batch_decode(generated_ids)
+    generated_confidence_values = response.parse_confidences(generated_texts, granular_confidence)
+    old_generated_confidence_values = generated_confidence_values.copy()
+    is_confidence_randomly_replaced = [False] * len(generated_confidence_values)
+
+    if random.random() < chance_to_change_confidence:  # Replace with random confidence
+        (
+            generated_ids,
+            generated_texts,
+            generated_confidence_values,
+            old_generated_confidence_values,
+            is_confidence_randomly_replaced,
+        ) = _handle_random_confidence_replacement(
+            tokenizer,
+            generated_texts,
+            generated_confidence_values,
+            device=device,
+            granular_confidence=granular_confidence,
+        )
+
+    generated_answer_labels = response.parse_binary_labels(generated_texts)
+
+    scores = [
+        reward.generated_answer_and_confidence_to_reward(
+            generated_answer_label,
+            generated_confidence_value,
+            ground_truth_label,
+            granular_confidence,
+            reward_function=reward_function,
+        )
+        for generated_answer_label, generated_confidence_value, ground_truth_label in zip(
+            generated_answer_labels, generated_confidence_values, labels.bool().tolist()
+        )
+    ]
+
+    scores = t.cast(
+        list[torch.FloatTensor],
+        [torch.tensor(s).to(device) for s in scores],
+    )
+
+    ######### 5.7 Take a PPO optimization step #########
+    reformulate_query_and_response_fn = functools.partial(
+        postprocessing.reformulate_query_and_response_for_binary_qa, tokenizer=tokenizer
+    )
+    reformulated_query_and_responses: list[postprocessing.ReformulatedQueryAndResponseDict] = [
+        reformulate_query_and_response_fn(query_ids=cur_input_ids, response=cur_response_text)
+        for cur_input_ids, cur_response_text in zip(input_ids_list, generated_texts)
+    ]
+    model.train()
+    model.gradient_checkpointing_enable()
+    ppo_queries = t.cast(
+        list[torch.LongTensor],
+        [
+            reformulated_query_and_response["query_ids"]
+            for reformulated_query_and_response in reformulated_query_and_responses
+        ],
+    )
+    ppo_responses = t.cast(
+        list[torch.LongTensor],
+        [
+            reformulated_query_and_response["response_ids"]
+            for reformulated_query_and_response in reformulated_query_and_responses
+        ],
+    )
+    stats = ppo_trainer.multimodal_step(
+        queries=ppo_queries,
+        responses=ppo_responses,
+        scores=scores,
+        images=images,
+    )
+
+    ######### 5.8 Create a report and log the training stats #########
+    input_texts_list = tokenizer.batch_decode(
+        postprocessing.replace_image_token_with_another_token_for_list_of_tensors(input_ids_list)
+    )
+    queries_with_gt_labels = [
+        f"(GT Label: {str(labels.bool().tolist()[idx])}) - {input_prompt}"
+        for idx, input_prompt in enumerate(input_texts_list)
+    ]
+
+    table_rows = []
+    if accumulating_game_logs:
+        table_rows = logging.log_custom_metrics_for_binary_qa(
+            tokenizer,
+            accumulating_game_logs,
+            input_ids,
+            labels,
+            generated_texts,
+            generated_confidence_values,
+            old_generated_confidence_values,
+            is_confidence_randomly_replaced,
+            generated_answer_labels,
+            scores,
+            ppo_responses,
+            stats,
+            queries_with_gt_labels,
+            log_calibration_plot,
+        )
+
+    ppo_trainer.log_stats(
+        stats=stats,
+        table_rows=table_rows,
+        column_names=[
+            "query",
+            "response",
+            "ppo_target_response",
+            "is_answer_correct",
+            "confidence",
+            "confidence_after_replacement",
+            "is_confidence_randomly_replaced",
+            "reward",
+        ],
+        rewards=scores,
+    )
+
+    return scores
