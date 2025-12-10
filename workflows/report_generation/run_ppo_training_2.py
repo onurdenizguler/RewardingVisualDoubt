e48fcae90e58af3e451b76136c7246654d99ba28
onurdenizguler <onurdenizguler@gmail.com>
2025-12-10 15:02:08 +0100

    chore(workflows): arrange result files

--- workflows/report_generation/run_ppo_training_2.py
+++
@@ -1,9 +0,0 @@
-from RewardingVisualDoubt import training
-
-from radialog_report_generation_ppo_training import train
-
-CONFIG_FILE = "/home/guests/deniz_gueler/repos/RewardingVisualDoubt/workflows/report_generation/configs/shifted_reward_config.yaml"
-
-if __name__ == "__main__":
-    metaparamaters, hyperparameters = training.load_default_configs(CONFIG_FILE)
-    train(metaparamaters, hyperparameters)
